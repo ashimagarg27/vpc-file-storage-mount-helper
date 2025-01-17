8227b01fd6ec2593aa5155c59865347bac858329
Ashima-Ashima1 <Ashima.Ashima1@ibm.com>
2025-01-17 14:57:12 +0530

    cos apis

--- mount-helper-container/server/server.go
+++ mount-helper-container/server/server.go
@@ -296,7 +296,7 @@ func handleCosMount() gin.HandlerFunc {
 		// pvcName := c.Query("pvc")
 		// pvcNamespace := c.Query("pvcNamespace")
 
-		os.Setenv("KUBERNETES_SERVICE_HOST", "172.20.0.1")
+		os.Setenv("KUBERNETES_SERVICE_HOST", "172.21.0.1")
 		os.Setenv("KUBERNETES_SERVICE_PORT", "443")
 
 		k8sClient, err := createK8sClient()
